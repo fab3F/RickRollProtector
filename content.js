beb280ae1e33934b51d87117405db0c887875511
fab3F <96261710+fab3F@users.noreply.github.com>
2024-12-24 16:56:59 +0100

    0.1.1.0 release: notification for old version on website

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 function edit() {
     var element = document.getElementById("rickelement000");
     if (element) {
-        element.innerHTML = "yes";
+        element.innerHTML = "0.1.1.0";
     }
 }
 document.addEventListener("DOMContentLoaded", edit);
